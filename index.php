2dc487f16d951fbd798d8edc2c253d960ac71b3a
Niik18 <ermolin.nik12@mail.com>
2026-09-09 21:24:48 +0300

    fun sum

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Функция суммы
+        function getSum(array $numbers) : int {
+            $sum = 0;
+            for ($i=0; $i < count($numbers); $i++) { 
+                $sum+= $numbers[$i];
+            }
+            return $sum;
+        }
+
+        $numbers = [2, 5, 7, 3];
+        echo getSum($numbers);
+    ?>
+</body>
+</html>

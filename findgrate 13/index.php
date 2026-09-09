7bd0ce41fc18a9addd8b989fa7dcd5db1e6544e2
Niik18 <ermolin.nik12@mail.com>
2026-09-09 22:48:22 +0300

    fun find greater then

---
+++ findgrate 13/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <?php
+        function filterGreaterThan(array $numbers, int $limit) : array {
+            $result = [];
+            for ($i=0; $i < count($numbers); $i++) { 
+                if ($numbers[$i] > $limit) {
+                    $result[] = $numbers[$i];
+                }
+            }
+            return $result;
+        }
+
+        $numbers = [4, 10, 2, 15, 7];
+        print_r(filterGreaterThan($numbers, 6));
+    ?>
+</body>
+</html>

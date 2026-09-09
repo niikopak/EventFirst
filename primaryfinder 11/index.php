44e8fdab5f3155b387a82dcf20a0e38f4e3c01c3
Niik18 <ermolin.nik12@mail.com>
2026-09-09 22:40:31 +0300

    fun finder primary

---
+++ primaryfinder 11/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>finder primarykey</title>
+</head>
+<body>
+    <?php
+        function finderPrimaryNum(array $numbers) : array {
+            $primaryNums = [];
+            for ($i=0; $i < count($numbers); $i++) {
+                $found = false;
+                for ($j=0; $j < count($primaryNums); $j++) { 
+                    if ($numbers[$i] == $primaryNums[$j]) {
+                        $found = true;
+                        break;
+                    }
+                }
+                if (!$found) {
+                    $primaryNums[] = $numbers[$i];
+                }
+            }
+            return $primaryNums;
+        }
+
+        $numbers = [1, 2, 2, 3, 1, 4, 3];
+        print_r(finderPrimaryNum($numbers));
+    ?>
+</body>
+</html>

a2d3cc960d3fda6fd608e6760e23ef0f04e80ef3
Niik18 <ermolin.nik12@mail.com>
2026-09-09 21:37:59 +0300

    fun average

--- average/index.php
+++ average/index.php
@@ -6,6 +6,20 @@
     <title>average</title>
 </head>
 <body>
-    
+    <?php
+        function getSum(array $numbers) : int {
+            $sum = 0;
+            for ($i=0; $i < count($numbers); $i++) { 
+                $sum+= $numbers[$i];
+            }
+            return $sum;
+        }
+        function getAverage(array $numbers) : int {
+            return getSum($numbers) / count($numbers);
+        }
+
+        $numbers = [10, 20, 30, 40];
+        echo getAverage($numbers);
+    ?>
 </body>
 </html>

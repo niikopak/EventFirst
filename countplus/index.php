a37b7a4fd67c500129b258fee5198976f58fccaa
Niik18 <ermolin.nik12@mail.com>
2026-09-09 21:59:11 +0300

    fun plus

---
+++ countplus/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>plus</title>
+</head>
+<body>
+    <?php
+        function countPositive(array $numbers) : int {
+            $positive = 0;
+            for ($i=0; $i < count($numbers); $i++) { 
+                if ($numbers[$i] > 0) {
+                    $positive++;
+                }
+            }
+            return $positive;
+        }
+        
+        $numbers = [-2, 5, 0, 7, -1, 3];
+        echo countPositive($numbers);
+    ?>
+</body>
+</html>
